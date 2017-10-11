407864826ffb8153f449d96b340a828e4a31ee83
Fabian <nowhere@telenet.be>
2017-10-11 03:27:08 +0200

    prototype 1 + images added

--- _gulp/tasks/scss.js
+++ _gulp/tasks/scss.js
@@ -21,7 +21,7 @@ var processors = [
 ];
 
 // task
-gulp.task('scss', ['dependencies'], function () {
+gulp.task('scss', [], function () {
     gulp.src(config.src)
         .pipe(sourcemaps.init())
         .pipe(sass.sync(config.settings)
